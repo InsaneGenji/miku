0b83ffc70a898472d334d81f3b92ec1b5c5574d4
LAPTOP-098N69UF\12550 <1255012902@qq.com>
2020-08-04 10:11:29 +0800

    改回了vue

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+	<div id="app">
+		<Miku />
+	</div>
+</template>
+
+<script>
+	import Miku from './views/Miku.vue';
+
+	export default {
+		name: 'App',
+		components: {
+			Miku
+		}
+	}
+</script>
+
+<style lang="scss">
+	body {
+		background: url(./assets/bg-miku.jpg) no-repeat center center;
+		background-size: cover;
+		background-attachment: fixed;
+	}
+</style>
